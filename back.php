f9feb0199753980f73e396494f4fe8397ac161bd
Anto <anto@Jakarta.local>
2018-07-19 10:39:55 +0700

    19Juli17

---
+++ back.php
@@ -0,0 +1,5 @@
+<?php
+	$text = $_POST['content'];
+
+	var_dump($text);
+?>
---
+++ index22.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/css/froala_editor.min.css' rel='stylesheet' type='text/css' />
+	<link href='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/css/froala_style.min.css' rel='stylesheet' type='text/css' />
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/js/froala_editor.min.js'></script>
+</head>
+
+<body>
+	<textarea id="froala-editor">Initialize the Froala WYSIWYG HTML Editor on a textarea.</textarea>
+	
+</body>
+
+<script>
+  $(document).ready(function() {
+        $('textarea#froala-editor').froalaEditor();
+    })
+</script>
+
+
+</html>
